3ee06f2b2eaa4b1ff2428a21dcc1ec0b94dc77d2
bxrrosz <davybarros831@gmail.com>
2025-07-15 17:13:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,10 +20,12 @@ if ativos_str:
         else:
             st.warning(f"Ticker '{t}' não encontrado ou inválido.")
 
-    if precos:
-        df_precos = pd.DataFrame(precos)
-        st.subheader("Preço Ajustado dos Ativos (últimos 6 meses)")
-        st.line_chart(df_precos)
+   if precos:
+    df_precos = pd.concat(precos.values(), axis=1)
+    df_precos.columns = precos.keys()
+    st.subheader("Preço Ajustado dos Ativos (últimos 6 meses)")
+    st.line_chart(df_precos)
+
     else:
         st.error("Nenhum dado válido foi carregado. Verifique os tickers.")
 
