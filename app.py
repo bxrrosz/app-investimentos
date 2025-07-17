a160d5bc6e0f758a7902fa5fed827aab7c6f9329
bxrrosz <davybarros831@gmail.com>
2025-07-17 11:43:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -271,7 +271,7 @@ if ativos_str:
                 try:
                     model = ARIMA(serie_previsao, order=(2, 1, 2))
                     model_fit = model.fit()
-                    previsao = model_fit.get_forecast(steps=10)
+                    previsao = model_fit.get_forecast(steps=30)
                     previsao_df = previsao.summary_frame()
 
                     fig = go.Figure()
